bb5b0f6d9fd0527a805ad11a42cd63df233cc3b9
DaniGoodwin <danmarkhunter13072006@gmail.com>
2021-07-06 12:20:34 +0300

    Brought comments back

--- declarations/ItemAnimHelper.d.ts
+++ declarations/ItemAnimHelper.d.ts
@@ -8,7 +8,29 @@ declare namespace IAHelper {
     const itemAnims: {
         [key: string]: IAnimTicker;
     };
+    /**
+     * Creates simple textures from given animated item texture from PC ('tall' texture)
+     * @param srcPath path to the 'tall' texture from the mod directory, simple textures will be put into 'assets/items-opaque/' folder.
+     * @param srcName name of the 'tall' texture without .png
+     * @param resultPath location of result textures, must be "<resource_directory>/items-opaque/"
+     * @param resultName name of the result textures (they will be with different meta and same name)
+     */
     function convertTexture(srcPath: string, srcName: string, resultPath: string, resultName: string): void;
+    /**
+     * Item texture will animate according to interval in ticks
+     * @param id id of the item you want to animate
+     * @param textureName name of your item's texture (you were putting it as resultName in 'convertTexture' function)
+     * @param ticks how many ticks must pass between changing item texture animation frame
+     * @param frames how many frames has the item texture animation
+     */
     function makeCommonAnim(id: number, textureName: string, ticks: number, frames: number): void;
+    /**
+     * Item texture will change its frames according to frame numbers array which you will specify
+     * @param id id of the item you want to animate
+     * @param textureName name of your item's texture (you were putting it as resultName in 'convertTexture' function)
+     * @param interval interval between which the texture will change its frame
+     * @param frames frames that will texture be being changed to every update interval
+     * @param intervals set of different intervals between which will animate the texture
+     */
     function makeAdvancedAnim(id: number, textureName: string, interval: number, frames: number[]): void;
 }
--- library/ItemAnimHelper.js
+++ library/ItemAnimHelper.js
@@ -1,12 +1,37 @@
+/// <reference path="../../declarations/core-engine.d.ts" />
+/*
+██╗████████╗███████╗███╗   ███╗ █████╗ ███╗  ██╗██╗███╗   ███╗██╗  ██╗███████╗██╗     ██████╗ ███████╗██████╗
+██║╚══██╔══╝██╔════╝████╗ ████║██╔══██╗████╗ ██║██║████╗ ████║██║  ██║██╔════╝██║     ██╔══██╗██╔════╝██╔══██╗
+██║   ██║   █████╗  ██╔████╔██║███████║██╔██╗██║██║██╔████╔██║███████║█████╗  ██║     ██████╔╝█████╗  ██████╔╝
+██║   ██║   ██╔══╝  ██║╚██╔╝██║██╔══██║██║╚████║██║██║╚██╔╝██║██╔══██║██╔══╝  ██║     ██╔═══╝ ██╔══╝  ██╔══██╗
+██║   ██║   ███████╗██║ ╚═╝ ██║██║  ██║██║ ╚███║██║██║ ╚═╝ ██║██║  ██║███████╗███████╗██║     ███████╗██║  ██║
+╚═╝   ╚═╝   ╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚══╝╚═╝╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝     ╚══════╝╚═╝  ╚═╝
+*/
 LIBRARY({
     name: "ItemAnimHelper",
     version: 2,
     shared: false,
     api: 'CoreEngine'
 });
+// © vstannumdum 2020
+//YouTube - DMH (Russian);
+//VK - https://www.vk.com/vstannumdum
+//VK Public - https://www.vk.com/dmhmods
+//report the bugs in the VK public - https://www.vk.com/dmhmods
+//This library was made to convert animated item textures from PC to common textures, 
+//and also to make item animations for Inner Core themselves.
+//Эта библиотека сделана, чтобы конвертировать текстуры анимированных предметов с ПК в обычные текстуры, 
+//а также делать сами предметные анимации для Inner Core.
 var IAHelper;
 (function (IAHelper) {
     IAHelper.itemAnims = {};
+    /**
+     * Creates simple textures from given animated item texture from PC ('tall' texture)
+     * @param srcPath path to the 'tall' texture from the mod directory, simple textures will be put into 'assets/items-opaque/' folder.
+     * @param srcName name of the 'tall' texture without .png
+     * @param resultPath location of result textures, must be "<resource_directory>/items-opaque/"
+     * @param resultName name of the result textures (they will be with different meta and same name)
+     */
     function convertTexture(srcPath, srcName, resultPath, resultName) {
         var anim = FileTools.ReadImage(__dir__ + "/" + srcPath + srcName + ".png");
         for (var i = 0; i < anim.getHeight() / anim.getWidth(); i++) {
@@ -20,6 +45,13 @@ var IAHelper;
         }
     }
     IAHelper.convertTexture = convertTexture;
+    /**
+     * Item texture will animate according to interval in ticks
+     * @param id id of the item you want to animate
+     * @param textureName name of your item's texture (you were putting it as resultName in 'convertTexture' function)
+     * @param ticks how many ticks must pass between changing item texture animation frame
+     * @param frames how many frames has the item texture animation
+     */
     function makeCommonAnim(id, textureName, ticks, frames) {
         if (!!IAHelper.itemAnims[textureName])
             return Logger.Log("An error occured calling 'ItemAnimHelper.makeCommonAnim' method. Another animation is already bound to item '" + Item.getName(id, 0) + "'", "ItemAnimHelper ERROR");
@@ -45,6 +77,14 @@ var IAHelper;
         });
     }
     IAHelper.makeCommonAnim = makeCommonAnim;
+    /**
+     * Item texture will change its frames according to frame numbers array which you will specify
+     * @param id id of the item you want to animate
+     * @param textureName name of your item's texture (you were putting it as resultName in 'convertTexture' function)
+     * @param interval interval between which the texture will change its frame
+     * @param frames frames that will texture be being changed to every update interval
+     * @param intervals set of different intervals between which will animate the texture
+     */
     function makeAdvancedAnim(id, textureName, interval, frames) {
         if (!!IAHelper.itemAnims[textureName])
             return Logger.Log("An error occured calling 'IAHelper.makeAdvancedAnim' method. Another animation is already bound to item '" + Item.getName(id, 0) + "'.", "ItemAnimHelper ERROR");
